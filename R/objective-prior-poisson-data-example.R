8ae8549786a04f5abdea681d23a4041e328ae4fc
rtrangucci <robert.trangucci@gmail.com>
2020-11-06 15:27:09 -0500

    Sampler parameters inline with server script

--- R/objective-prior-poisson-data-example.R
+++ R/objective-prior-poisson-data-example.R
@@ -2,38 +2,40 @@ library(cmdstanr)
 library(loo)
 mod <- cmdstan_model('stan/objective-prior-poisson-lik.stan')
 y <- readRDS('data/pois_draws_n_20_mean_1.35.RDS')
-dat <- list(c = 2, u_0 = 0.65)
+dat <- list(c = 2, u_0 = 0.65, acc = 1e-11)
 dat$y <- y
 dat$N <- length(y)
 fit <- mod$sample(dat, 
-                  iter_warmup =  2000,
-                  iter_sampling =  10000,
+                  iter_warmup =  8000,
+                  iter_sampling =  2500 * 40,
                   parallel_chains = 4,
                   chains = 4,
                   adapt_delta = 0.99,
                   max_treedepth = 12,
                   inv_metric = as.array(0.0407221),
-                  step_size = 0.01,
-                  adapt_engaged = F,
+                  step_size = 0.3,
+                  adapt_engaged = T,
                   refresh = 5e3,
+                  thin = 40,
                   seed = 123,
                   init = function() list(theta = runif(1,min=0.5,max=1.0))
         )
 d <- fit$draws()
 
-dat <- list(c = 2, u_0 = 0.1)
+dat <- list(c = 2, u_0 = 0.1, acc = 1e-9)
 dat$y <- y
-dat$N <- N
+dat$N <- length(y) 
 fit <- mod$sample(dat, 
-                  iter_warmup =  2000,
-                  iter_sampling =  10000,
+                  iter_warmup =  8000,
+                  iter_sampling =  2500 * 10,
                   parallel_chains = 4,
                   chains = 4,
                   adapt_delta = 0.99,
                   max_treedepth = 12,
                   inv_metric = as.array(0.0407221),
-                  step_size = 0.05,
-                  adapt_engaged = F,
+                  step_size = 0.2,
+                  adapt_engaged = T,
+                  thin = 10,
                   refresh = 5e3,
                   seed = 123,
                   init = function() list(theta = runif(1,min=0.5,max=2.0))
